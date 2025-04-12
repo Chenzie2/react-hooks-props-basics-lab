11631e4e6e00d60d44c28adec6bda06855beb709
g_zawie <gzawie24@gmail.com>
2025-04-12 12:11:31 +0300

    Pass props to Home and About; conditionally render bio; add Links component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,29 @@
 import React from "react";
 
-function About() {
+function About({ bio, github, linkedin }) {
+  // Define Links component inside About
+  function Links({ github, linkedin }) {
+    return (
+      <div>
+        <h3>Links</h3>
+        <a href={github}>{github}</a>
+        <br />
+        <a href={linkedin}>{linkedin}</a>
+      </div>
+    );
+  }
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {/* Conditionally render bio */}
+      {bio && <p>{bio}</p>}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      {/* Render Links */}
+      <Links github={github} linkedin={linkedin} />
     </div>
   );
 }
