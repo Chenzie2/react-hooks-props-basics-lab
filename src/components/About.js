8e7cc9cd126df4770e4ea2a05c277afa56994c52
g_zawie <gzawie24@gmail.com>
2025-04-12 12:20:18 +0300

    Fix props and imports in App.js to pass user data correctly

--- src/components/About.js
+++ src/components/About.js
@@ -1,28 +1,12 @@
 import React from "react";
+import Links from "./Links";
 
 function About({ bio, github, linkedin }) {
-  // Define Links component inside About
-  function Links({ github, linkedin }) {
-    return (
-      <div>
-        <h3>Links</h3>
-        <a href={github}>{github}</a>
-        <br />
-        <a href={linkedin}>{linkedin}</a>
-      </div>
-    );
-  }
-
   return (
     <div id="about">
       <h2>About Me</h2>
-
-      {/* Conditionally render bio */}
       {bio && <p>{bio}</p>}
-
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-
-      {/* Render Links */}
       <Links github={github} linkedin={linkedin} />
     </div>
   );
